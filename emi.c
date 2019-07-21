4893d0155dacf4e9c428c5f20375c0fe3cc77d66
Centrix14 <artiom.zvyagin@yandex.ru>
2019-07-21 12:06:52 +0300

    new embedded interpreter mat

---
+++ emi.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "mat.h"
+#include "mattok.h" 
+
+/*
+ * Embedded interpreter mat 
+ * It can be used to interpret programs on mat in other programs.
+ * v0.1
+ * by Centrix 21.07.2019
+ */
+
+void matrun(char *prog) {
+	char *tok = gettoken(prog, 1);
+
+	while ( tok != NULL ) {
+		interpret(tok);
+		tok = gettoken(prog, 0);
+	}
+}
+
+void matfrun(char *fname) {
+	FILE *source = fopen(fname, "r");
+	char *tok, line[256];
+
+	if ( source == NULL ) {
+		fprintf(stderr, "Can`t open `%s`\n", fname);
+		return;
+	}
+
+	fgets(line, 256, source);
+	while ( !feof(source) ) {
+		tok = gettoken(line, 1);
+
+		while (tok != NULL) {
+			interpret(tok);
+			tok = gettoken(line, 0);
+		}
+		fgets(line, 256, source);
+	}
+
+	fclose(source);
+}
---
+++ emi.h
@@ -0,0 +1,14 @@
+#ifndef __MAT_EMI_H_INCLUDED__
+#define __MAT_EMI_H_INCLUDED__
+
+/*
+ * Embedded interpreter mat
+ * This file is attached to emi.c
+ * v0.1
+ * by Centrix 21.07.2019
+ */
+
+void matrun(char *prog);
+void matfrun(char *fname);
+
+#endif
